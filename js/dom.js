c5f8721cde7fa21f69538a32217601ba4ccaec70
unknown <mitutomap@gmail.com>
2022-09-13 19:47:12 +0600

    comment

---
+++ js/dom.js
@@ -0,0 +1,5 @@
+document.getElementById('apply-bg').addEventListener('click', function () {
+  //console.log('apply')
+  const friends = document.getElementsByClassName('friend');
+  console.log(friends);
+})
